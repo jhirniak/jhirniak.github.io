3b19847ac8bacd158027f535dd71047a6607d391
Jaroslaw Hirniak <j@hirniak.info>
2015-11-08 14:05:01 +0000

    rebuilding site Sun Nov  8 14:05:01 GMT 2015

--- generate.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-hugo -b=http://blog.hirniak.com/ --theme=hugo-steam-theme --buildDrafts -d=../blog.hirniak.info-static/ -v
-echo "rsync -avxz * ravanave@s3.mydevil.net:/home/ravanave/domains/blog.hirniak.com/public_html"
