c23cfc3d28d86bc1e8d92b4b225797c5fe197651
Jaroslaw Hirniak <j@hirniak.info>
2015-11-10 10:55:30 +0000

    rebuilding site Tue Nov 10 10:55:30 GMT 2015

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,11 @@
 
 # Build the project.
 # hugo -t hugo-steam-theme
-echo -e "\e[1m\e[7m\e[Building the project...\e[0m"
+echo -e "\e[1m\e[7m\e[32mBuilding the project...\e[0m"
 hugo -b=http://jhirniak.github.io/ --theme=hugo-steam-theme -d=public/ -v
 
 # Add changes to git.
-echo -e "\e[1m\e[7m\e[Committing updates to code branch...\e[0m"
+echo -e "\e[1m\e[7m\e[32mCommitting updates to code branch...\e[0m"
 git add -A
 
 # Commit changes.
@@ -19,6 +19,6 @@ git commit -m "$msg"
 # Push source and build repos.
 git push origin code
 
-echo -e "\e[1m\e[7m\e[Pushing the changes to master branch...\e[0m"
-echo -e "\e[1m\e[7m\e[Updating the website...\e[0m"
+echo -e "\e[1m\e[7m\e[32mPushing the changes to master branch...\e[0m"
+echo -e "\e[1m\e[7m\e[32mUpdating the website...\e[0m"
 git subtree push --prefix=public git@github.com:jhirniak/jhirniak.github.io.git master
